d42416f55f00b1a8853418bfa578e89da2f80559
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-03-28 16:28:14 -0300

    Adicionando migrations

---
+++ Server/app/Http/Controllers/CategoriaControllerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoriaControllerRequest;
+use App\Http\Requests\UpdateCategoriaControllerRequest;
+use App\Models\CategoriaController;
+
+class CategoriaControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoriaControllerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoriaController $categoriaController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoriaController $categoriaController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoriaControllerRequest $request, CategoriaController $categoriaController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoriaController $categoriaController)
+    {
+        //
+    }
+}
---
+++ Server/app/Http/Controllers/StatusControllerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStatusControllerRequest;
+use App\Http\Requests\UpdateStatusControllerRequest;
+use App\Models\StatusController;
+
+class StatusControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStatusControllerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StatusController $statusController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StatusController $statusController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStatusControllerRequest $request, StatusController $statusController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StatusController $statusController)
+    {
+        //
+    }
+}
