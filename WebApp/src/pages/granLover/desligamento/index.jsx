05bbaa51ce52aa0409b796eb03f16ab9ff3daecc
Breno Maini <dev.brenomaini@gmail.com>
2023-04-06 14:45:22 -0300

    Commit pre acesso TW

--- WebApp/src/pages/granLover/desligamento/index.jsx
+++ WebApp/src/pages/granLover/desligamento/index.jsx
@@ -17,7 +17,9 @@ export default function desligamento() {
     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
       {/*header*/}
       <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-        <h3 className="text-3xl font-semibold">Desligamento</h3>
+        <h3 className="text-3xl font-semibold text-center justify-center flex w-full">
+          Desligamento
+        </h3>
         <button
           className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
           onClick={() => setShowModal(false)}
@@ -30,7 +32,7 @@ export default function desligamento() {
       {/*body*/}
       <div className="relative p-6 flex-auto">
         <p className="my-4 text-slate-500 text-lg leading-relaxed">
-          Desligamento
+          Insira o e-mail do colaborador desligado.
         </p>
         <div className="flex w-full justify-around flex-wrap h-96 items-center ">
           <Input
@@ -44,32 +46,13 @@ export default function desligamento() {
       {/*footer*/}
       <div className="flex items-center justify-around p-6 border-t border-solid border-slate-200 rounded-b">
         <button
-          className="text-white bg-gran-red bg-opacity-80 font-bold uppercase px-6 py-2 text-sm rounded mr-1 mb-1 ease-linear transition-all duration-150 hover:scale-105"
-          type="button"
-          onClick={() => setShowModal(false)}
-        >
-          Cancelar
-        </button>
-        <button
           className="text-white bg-gran-blue bg-opacity-90 font-bold uppercase px-6 py-2 text-sm rounded mr-1 mb-1 ease-linear transition-all duration-150 hover:scale-105"
           type="button"
           onClick={() => {
-            alert(
-              "Items retirados para: " +
-                valueRec +
-                "Aprovado por: " +
-                valueApr +
-                "Patrimônio:" +
-                valuePat +
-                "Item:" +
-                item.item +
-                "Setor:" +
-                item.setor
-            );
-            setShowModal(false);
+            alert("O colaborador " + inputs.emailDesligado);
           }}
         >
-          Retirar
+          Desvincular
         </button>
       </div>
     </div>
