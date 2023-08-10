aee8d65f67b104a052373d0773122a8149ec7d6e
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-08-10 15:20:07 -0300

    dbseed

--- Server/database/seeders/ItemSeeder.php
+++ Server/database/seeders/ItemSeeder.php
@@ -16,8 +16,7 @@ class ItemSeeder extends Seeder
             'marca_id' => '1',
             'categoria_id' => '1',
             'nome' => 'Dell G15 Gamer',
-            'descricao' => '12ª geração Intel® Core™ i5-12500H, Windows 11 Pro, NVIDIA® GeForce® RTX™ 3050, 4GB GDDR6, Full HD de 15.6" (1920 x 1080), 120Hz, Memória de 8GB DDR5 (1x8GB) 4800MHz; Expansível até 32GB (2 slots soDIMM), SSD de 256GB PCIe NVMe M.2',
-            'alerta_quantidade' => '0'
+            'descricao' => '12ª geração Intel® Core™ i5-12500H, Windows 11 Pro, NVIDIA® GeForce® RTX™ 3050, 4GB GDDR6, Full HD de 15.6" (1920 x 1080), 120Hz, Memória de 8GB DDR5 (1x8GB) 4800MHz; Expansível até 32GB (2 slots soDIMM), SSD de 256GB PCIe NVMe M.2'
         ]);
     }
 }
