ade3b4ba471a8aba00f1a69c28f372f2898767b3
Breno Maini <dev.brenomaini@gmail.com>
2023-08-11 14:32:34 -0300

    Correçao layout

--- WebApp/src/modules/Controles/items/page/index.jsx
+++ WebApp/src/modules/Controles/items/page/index.jsx
@@ -39,40 +39,46 @@ export default function itens() {
       ) : isError ? (
         <div>Error: {error.message}</div>
       ) : (
-        <div className="table gap-2 row-auto h-16 w-full  text-center p-2">
-          <HeaderItens />
-          {data.data.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="table-row-group group text-center border even:bg-black even:bg-opacity-20"
-              >
-                <div className="table-row group-hover:bg-gran-blue group-hover:bg-opacity-25 ">
-                  <div className="table-cell">{item.numeroSerie || "N/A"} </div>
-                  <div className="table-cell">{item.patrimonio || "N/A"} </div>
-                  <div className="table-cell">{item.item.nome} </div>
-                  <div
-                    className={
-                      item.status === "DISPONÍVEL" ||
-                      item.status === "Disponível"
-                        ? "bg-green border-b font-semibold"
-                        : item.status === "QUEBRADO"
-                        ? "bg-gran-red text-white border-b border-black font-semibold"
-                        : "bg-orange border-b font-semibold"
-                    }
-                  >
-                    {item.status.toUpperCase()}
-                  </div>
-                  <div className="table-cell"> {item.localizacao} </div>
-                  <div className="table-cell"> {item.setor.nome} </div>
-                  <div className="table-cell"> {item?.responsavel} </div>
-                  <div className="table-cell ">
-                    <EditarResponsavel item={item} />
+        <>
+          <div className="table gap-2 row-auto h-16 w-full  text-center p-2">
+            <HeaderItens />
+            {data.data.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="table-row-group group text-center border even:bg-black even:bg-opacity-20"
+                >
+                  <div className="table-row group-hover:bg-gran-blue group-hover:bg-opacity-25 ">
+                    <div className="table-cell">
+                      {item.numeroSerie || "N/A"}{" "}
+                    </div>
+                    <div className="table-cell">
+                      {item.patrimonio || "N/A"}{" "}
+                    </div>
+                    <div className="table-cell">{item.item.nome} </div>
+                    <div
+                      className={
+                        item.status === "DISPONÍVEL" ||
+                        item.status === "Disponível"
+                          ? "bg-green border-b font-semibold"
+                          : item.status === "QUEBRADO"
+                          ? "bg-gran-red text-white border-b border-black font-semibold"
+                          : "bg-orange border-b font-semibold"
+                      }
+                    >
+                      {item.status.toUpperCase()}
+                    </div>
+                    <div className="table-cell"> {item.localizacao} </div>
+                    <div className="table-cell"> {item.setor.nome} </div>
+                    <div className="table-cell"> {item?.responsavel} </div>
+                    <div className="table-cell ">
+                      <EditarResponsavel item={item} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
           <BotaoNextPrev
             pagina={page}
             setPrevPagina={setPrevPagina}
@@ -80,7 +86,7 @@ export default function itens() {
             setPrimeiraPagina={setPrimeiraPagina}
             setUltimaPagina={setPage}
           />
-        </div>
+        </>
       )}
     </>
   );
