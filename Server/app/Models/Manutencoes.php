fe2f9960c6258b6bdf1a3ac00a7fc1e22f132f51
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-08-16 13:51:58 -0300

    add rules no model manutencoes

--- Server/app/Models/Manutencoes.php
+++ Server/app/Models/Manutencoes.php
@@ -16,6 +16,27 @@ class Manutencoes extends Model
         'descricao'
     ];
 
+    public function rules()
+    {
+        return [
+            'itens_estoque_id' => 'exists:itens_estoque,id',
+            'descricao' => 'required'
+        ];
+
+        /*
+            1) tabela
+            2) nome da coluna que será pesquisada na tabela3
+            3) id do registro que será desconsiderado na pesquisa
+        */
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+        ];
+    }
+
     public function item_estoque()
     {
         //UMA manutenção PERTENCE a UM itemEstoque
